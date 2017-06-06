26d35bf4d8f94646dcb683efcf067479dbf50bd2
jithin <jithin.ks@paladion.net>
2017-06-06 22:38:27 +0530

    added second thread

--- casestudy1/src/casestudy1/casestudy1.java
+++ casestudy1/src/casestudy1/casestudy1.java
@@ -1,10 +1,28 @@
 package casestudy1;
 
-class Multi extends Thread{  
+class Multi1 extends Thread{
+	String[] ptr;
+	public Multi1(String[] ptr){
+		this.ptr=ptr;
+	}
+	public void run(){
+		int y=30;
+		if(y==20){
+			System.out.println("y is 30");
+			
+		}
+		else{
+			System.out.println(ptr.length);
+		}
+	}
+}
+
+class Multi extends Thread{ 
+	static String[] ptr;
 public void run(){  
 int x=20;
-String[] ptr;
-if(x==20){
+
+if(x==10){
 	ptr=null;
 }
 else{
@@ -13,8 +31,14 @@ else{
 System.out.println(ptr.length);
 
 }  
-public static void main(String args[]){  
+public static void main(String args[]) throws InterruptedException{  
 Multi t1=new Multi();  
-t1.start();  
+Multi1 t2=new Multi1(ptr);
+t1.start(); 
+t2.start();
+t1.join();
+t2.join();
  }  
-} 
+}
+
+
